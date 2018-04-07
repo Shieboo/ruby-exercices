14593b952414a7b2debb6ed83012dbad09c1c1f9
Adrien Pardo <35133260+Shieboo@users.noreply.github.com>
2018-04-07 21:07:06 +0200

    removed comments

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,16 +1,11 @@
 def caesar_cipher(string, shift)
-  # should return "Bmfy f xywnsl!"
   arr = string.split('')
-
   arr.map! do |char|
-    # manage uppercased character
     if (65..90) === char.ord
       char = char.ord + shift <= 90 ? (char.ord + shift).chr : (char.ord + shift - 26).chr
-    # manage downcased character
     else
       if (97..122) === char.ord
         char = char.ord + shift <= 122 ? (char.ord + shift).chr : (char.ord + shift - 26).chr
-      # other character
       else
         char
       end
@@ -20,10 +15,3 @@ def caesar_cipher(string, shift)
 end
 
 caesar_cipher('What a string!', 5)
-# should return "Bmfy f xywnsl!"
-
-# notes:
-# a = 97
-# z = 122
-# A = 65
-# Z = 90
